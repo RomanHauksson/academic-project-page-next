cad688fb78feb0ca8d4728a31777781ebd6e3f6f
Roman Hauksson-Neill <62364808+RomanHauksson@users.noreply.github.com>
2026-04-15 14:18:28 -0500

    add image zoom feature

--- components/image.tsx
+++ components/image.tsx
@@ -1,15 +1,21 @@
+"use client";
+
 import NextImage from "next/image";
 import type { ComponentProps } from "react";
+import Zoom from "react-medium-image-zoom";
 import { cn } from "@/lib/utils";
+import "react-medium-image-zoom/dist/styles.css";
 
 export function Image({
 	className,
 	...props
 }: ComponentProps<typeof NextImage>) {
 	return (
-		<NextImage
-			className={cn("rounded-lg max-w-full mx-auto", className)}
-			{...props}
-		/>
+		<Zoom classDialog="not-prose" ZoomContent={({ img }) => <>{img}</>}>
+			<NextImage
+				className={cn("rounded-lg max-w-full mx-auto not-prose", className)}
+				{...props}
+			/>
+		</Zoom>
 	);
 }
